ab2249c4d7405aac8ee6392ab080e592f9a63b5c
Nirajan995 <nirajankunwor604@gmail.com>
2022-01-13 19:25:54 +0545

    Client Side Auth

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
-import { ChannelContainer, ChannelListContainer } from "./components/index";
+import {
+  ChannelContainer,
+  ChannelListContainer,
+  Auth,
+} from "./components/index";
 
 import "./App.css";
 
@@ -10,7 +14,11 @@ const apiKey = "1160644";
 
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
 const App = () => {
+  if (!authToken) return <Auth />;
+
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
